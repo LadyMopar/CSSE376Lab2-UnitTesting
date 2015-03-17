0f801b1159562eb6dedc344576e3ccb12b4a4d57
LadyMopar <richarel@rose-hulman.edu>
2015-03-17 16:57:27 -0400

    Wrote Some Tests!

--- ExpediaTest/FlightTest.cs
+++ ExpediaTest/FlightTest.cs
@@ -7,6 +7,81 @@ namespace ExpediaTest
 	[TestFixture()]
 	public class FlightTest
 	{
-		//TODO Task 7 items go here
+
+        [Test()]
+        public void TestThatFlightInitializes()
+        {
+            var target = new Flight(new DateTime(15, 03, 02), new DateTime(15, 03, 09), 1200);
+            Assert.IsNotNull(target.Miles);
+        }
+
+        [Test()]
+        public void TestGetBasePriceOneDaySpread()
+        {
+            var target = new Flight(new DateTime(15, 04, 10), new DateTime(15, 04, 11), 700);
+            Assert.AreEqual(220, target.getBasePrice());
+        }
+
+        [Test()]
+        public void TestGetMilesOfFlight()
+        {
+            var target = new Flight(new DateTime(15, 12, 12), new DateTime(15, 12, 24), 1500);
+            Assert.AreEqual(target.Miles, 1500);
+        }
+
+
+        [Test()]
+        public void TestGetBasePriceOneWeekSpread()
+        {
+            var target = new Flight(new DateTime(15, 04, 10), new DateTime(15, 04, 17), 1700);
+            Assert.AreEqual(340, target.getBasePrice());
+        }
+
+        [Test()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestThatFlightThrowsOnBadRange()
+        {
+            new Flight(new DateTime(15, 11, 18), new DateTime(15, 11, 15), 100);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestThatCantUseNegativeMiles()
+        {
+            new Flight(new DateTime(15, 04, 10), new DateTime(15, 04, 17), -1700);
+        }
+
+        [Test()]
+        public void CheckAndSeeIfFlightsAreEqual()
+        {
+            var flight1 = new Flight(new DateTime(15, 04, 10), new DateTime(15, 04, 17), 1700);
+            var flight2 = new Flight(new DateTime(15, 04, 10), new DateTime(15, 04, 17), 1700);
+            Assert.AreEqual(flight1, flight2);
+        }
+
+        [Test()]
+        public void CheckAndSeeIfFlightsAreSimilarButNotSameObject()
+        {
+            var flight1 = new Flight(new DateTime(15, 04, 10), new DateTime(15, 04, 17), 1700);
+            var flight2 = new Flight(new DateTime(15, 04, 10), new DateTime(15, 04, 17), 1700);
+            Assert.AreNotSame(flight1, flight2);
+        }
+
+        [Test()]
+        public void CheckIfOverriddenEqualsFunctiomWorksForTrue()
+        {
+            var flight1 = new Flight(new DateTime(15, 04, 10), new DateTime(15, 04, 17), 1700);
+            var flight2 = new Flight(new DateTime(15, 04, 10), new DateTime(15, 04, 17), 1700);
+            Assert.True(flight1.Equals(flight2));
+        }
+
+        [Test()]
+        public void CheckIfOverriddenEqualsFunctiomWorksForFalse()
+        {
+            var flight1 = new Flight(new DateTime(15, 04, 11), new DateTime(15, 04, 17), 1700);
+            var flight2 = new Flight(new DateTime(15, 04, 10), new DateTime(15, 04, 17), 1700);
+            Assert.False(flight1.Equals(flight2));
+        }
+
 	}
 }
--- ExpediaTest/HotelTest.cs
+++ ExpediaTest/HotelTest.cs
@@ -7,6 +7,39 @@ namespace ExpediaTest
     [TestFixture()]
     public class HotelTest
     {
-		//TODO Task 6 items go here
+		private readonly int NightToRentHotel = 5;
+
+        [Test()]
+        public void TestThatHotelInititalizes()
+        {
+            var target = new Hotel(NightToRentHotel);
+            Assert.IsNotNull(target);
+        }
+
+        [Test()]
+        public void TestThatHotelHasCorrectBasePriceForOneDayStay()
+        {
+            var target = new Hotel(1);
+            Assert.AreEqual(45, target.getBasePrice());
+        }
+        [Test()]
+        public void TestThatHotelHasCorrectBasePriceForTwoDayStay()
+        {
+            var target = new Hotel(2);
+            Assert.AreEqual(90, target.getBasePrice());
+        }
+        [Test()]
+        public void TestThatHotelHasCorrectBasePriceForTenDaysStay()
+        {
+            var target = new Hotel(10);
+            Assert.AreEqual(450, target.getBasePrice());
+        }
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestThatHotelThrowsOnBadLength()
+        {
+            new Hotel(-5);
+        }
     }
 }
